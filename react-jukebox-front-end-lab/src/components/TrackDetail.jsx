c0ac0c2d312605e3c03a9a68b22d31866f93ca15
camdelay16 <camdelay16@gmail.com>
2024-11-13 10:55:56 -0500

    ready for review

---
+++ react-jukebox-front-end-lab/src/components/TrackDetail.jsx
@@ -0,0 +1,21 @@
+const TrackDetail = (props) => {
+  if (!props.selected) {
+    return (
+      <div>
+        <h1>No Details</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{props.selected.title}</h1>
+      <h2>{props.selected.artist}</h2>
+      <button onClick={() => props.handleFormView(props.selected)}>Edit</button>
+      <button onClick={() => props.handleRemoveTrack(props.selected._id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default TrackDetail;
---
+++ react-jukebox-front-end-lab/src/components/TrackList.jsx
@@ -0,0 +1,20 @@
+const TrackList = (props) => {
+  const tracks = props.trackList.map((track) => (
+    <a
+      key={track._id}
+      onClick={() => props.updateSelected(track)}
+    >
+      <li>{track.title}</li>
+    </a>
+  ));
+
+  return (
+    <div>
+      <h1>Track List</h1>
+      {!props.trackList.length ? <h2>No tracks yet!</h2> : <ul>{tracks}</ul>}
+      <button>{props.isFormOpen ? "Close Form" : "New Track"}</button>
+    </div>
+  );
+};
+
+export default TrackList;
